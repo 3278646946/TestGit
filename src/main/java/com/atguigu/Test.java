31a8a6d4dd3ec5712fbb510ef2985c124ce7bcb1
盘创铭 <13804176+pan-chuangming@user.noreply.gitee.com>
2025-03-05 16:46:05 +0800

    学习如何推送项目到Git Hub

---
+++ src/main/java/com/atguigu/Test.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @version 1.0
+ * @Author 盘创铭
+ * @Date 2025/3/5 下午4:40
+ * @注释
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("helle git");
+    }
+}
